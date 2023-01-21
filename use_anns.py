879f39bcfe18c78635294bb3e0b23e4180dadb96
Gregor Foitzik <79803660+GregorFelixFoitzik@users.noreply.github.com>
2023-01-21 16:29:00 +0100

    Add files via upload

--- use_anns.py
+++ use_anns.py
@@ -7,9 +7,19 @@ from scipy.stats import spearmanr
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f"\nThe ANN will run on the: {device}")
 
+algo_dic = {"probSAT":      ["probSAT", "ProbSAT-7SAT"],
+            "miniSAT":      ["miniSAT", "Minisat"],
+            "lingeling":    ["lingeling","Lingeling-CF"],
+            "clasp":        ["clasp", "Clasp-Rooks"],
+            "cplex_rcw":    ["cplex", "CPLEX-RCW2"],
+            "cplex_regi":   ["cplex", "CPLEX-Regions"]
+            }
+
+algo = "miniSAT"
+
 ### here is code example to test the saved network
-loaded_model = torch.load("ann_probSAT.pth")
-data_val = pd.read_csv("./ProbSAT-7SAT/Daten/data_val.csv", sep=";").drop("instance", axis=1)
+loaded_model = torch.load(f"ann_{algo_dic[algo][0]}.pth")
+data_val = pd.read_csv(f"./{algo_dic[algo][1]}/Daten/data_val.csv", sep=";").drop("instance", axis=1)
 data_val = EPM_Dataset(data_val)
 val_loader = DataLoader(data_val, batch_size=1, shuffle=False)  # LOCO is performed by setting the batch-size to 1
 
@@ -33,6 +43,6 @@ for ii, (inputs, actual) in enumerate(val_loader):
 
         test.append( ( np.log(actual_cpu_value) - np.log(preds_cpu_value) )**2 )
 
-print()
-print(np.sqrt( np.mean(test) ) )
+print(np.mean(test))
+print(np.sqrt( np.mean(test)  ) )
 #print(spearmanr(actuals, predictions))
